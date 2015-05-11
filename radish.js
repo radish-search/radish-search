410633c4121de5d90c5345119500acc08c86a738
Victor Kareh <vkareh@vkareh.net>
2015-05-11 16:27:39 -0400

    Add plugin architecture

--- radish.js
+++ radish.js
@@ -1,16 +1,22 @@
 var config = require('./lib/config');
 var Redis = require('./lib/redis');
+var Index = require('./lib/index');
 
 module.exports = function(options) {
   options = options || {};
   return new Redis(options.redis || config.get('redis'));
 }
 
-module.exports.Redis = Redis;
-module.exports.Index = require('./lib/index');
-
 var indexers = require('./indexers');
 var readers = require('./readers');
 
 for (var i in indexers) { module.exports[i] = indexers[i]; }
 for (var r in readers) { module.exports[r] = readers[r]; }
+
+var plugins = require('./lib/plugins');
+module.exports.loadPlugin = function(plugin) {
+  plugins.loadPlugin.call(this, plugin);
+}
+
+module.exports.Redis = Redis;
+module.exports.Index = Index;
